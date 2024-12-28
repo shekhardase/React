f769bc7810c9b8b1a43275a223f42f165780ae97
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-12-28 15:27:09 +0530

    Project Intials Setup done

---
+++ React_Webpage/PortFolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function App() {
+  return (
+
+    <div className="flex items-center w-full h-full justify-center">
+      <h1 className=" text-5xl font-thin  text-red-950 mt-8">
+        Hello world!
+      </h1>
+    </div>
+
+
+  )
+}
